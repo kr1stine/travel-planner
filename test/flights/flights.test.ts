40e7e9d5b648bee6147584a32b76801d5ba77c05
Kristine Leetberg <>
2021-02-16 16:11:02 +0200

    Added test for shortest route

--- test/flights/flights.test.ts
+++ test/flights/flights.test.ts
@@ -2,24 +2,37 @@ import app from "../../app/app";
 import { agent as request } from "supertest";
 import { expect } from "chai";
 
-let firstOriginTest = "TLL";
-let firstDestinationTest = "LAX";
+it(`should GET /flights/shortest?origin=TLL&destination=HEL`, async function () {
+  let origin = "TLL";
+  let destination = "HEL";
 
-let firstRouteBody = {
-  route: [
-    { origin: "TLL", destination: "HEL", type: "flight" },
-    { origin: "HEL", destination: "LAX", type: "flight" },
-  ],
-};
-
-it(`should GET /flights/shortest?origin=TLL&destination=LAX`, async function () {
+  let routeBody = {
+    route: [{ origin: "TLL", destination: "HEL", type: "flight" }],
+  };
   const res = await request(app)
-    .get(
-      `/flights/best?origin=${firstOriginTest}&destination=${firstDestinationTest}`
-    )
+    .get(`/flights/best?origin=${origin}&destination=${destination}`)
     .send();
   expect(res.status).to.equal(200);
   expect(res.body).not.to.be.empty;
   expect(res.body).to.be.an("object");
-  expect(res.body.route).to.be.equals(firstRouteBody.route);
+  expect(res.body.route).to.be.equals(routeBody.route);
+});
+
+it(`should GET /flights/shortest?origin=TLL&destination=LAX`, async function () {
+  let origin = "TLL";
+  let destination = "LAX";
+
+  let routeBody = {
+    route: [
+      { origin: "TLL", destination: "HEL", type: "flight" },
+      { origin: "HEL", destination: "LAX", type: "flight" },
+    ],
+  };
+  const res = await request(app)
+    .get(`/flights/best?origin=${origin}&destination=${destination}`)
+    .send();
+  expect(res.status).to.equal(200);
+  expect(res.body).not.to.be.empty;
+  expect(res.body).to.be.an("object");
+  expect(res.body.route).to.be.equals(routeBody.route);
 });
