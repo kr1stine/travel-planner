27d7140620f7c903e1278f55a658d6d10c83b2bc
Kristine Leetberg <>
2021-02-17 06:07:50 +0200

    Added self-transfer test

--- test/flights/flights.test.ts
+++ test/flights/flights.test.ts
@@ -72,3 +72,37 @@ it(`should GET /route/shortest?origin=TLL&destination=JFK`, async function () {
   expect(res.body).to.be.an("object");
   expect(res.body.path).to.be.equals(routeBody.path);
 });
+
+it(`should GET /route/shortest?origin=RIX&destination=ESS`, async function () {
+  let origin = "RIX";
+  let destination = "ESS";
+
+  let routeBody = {
+    path: [
+      {
+        origin: "RIX",
+        destination: "BRE",
+        type: "flight",
+      },
+      {
+        origin: "BRE",
+        destination: "FMO",
+        type: "flight",
+      },
+      {
+        origin: "FMO",
+        destination: "ESS",
+        type: "self-transfer",
+      },
+    ],
+    pathString: "RIX->BRE->FMO=>ESS",
+  };
+  const res = await request(app)
+    .get(`/route/shortest?origin=${origin}&destination=${destination}`)
+    .send();
+  expect(res.status).to.equal(200);
+  expect(res.body).not.to.be.empty;
+  expect(res.body).to.be.an("object");
+  expect(res.body.path).to.be.equals(routeBody.path);
+  expect(res.body.pathString).to.be.equals(routeBody.pathString);
+});
